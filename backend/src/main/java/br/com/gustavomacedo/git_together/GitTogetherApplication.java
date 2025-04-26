c68c80e6f6d93114bcc257183896ad1bb0afedbe
gustavomotamacedo <mota.macedo05@gmail.com>
2025-04-25 21:19:08 -0300

    feat: criando projeto spring boot

---
+++ backend/src/main/java/br/com/gustavomacedo/git_together/GitTogetherApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavomacedo.git_together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTogetherApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/gustavomacedo/git_together/GitTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavomacedo.git_together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
