0844b252b63e552950d8342ca66330f0f7972f92
gustavomotamacedo <mota.macedo05@gmail.com>
2025-04-25 23:27:27 -0300

    feat: criando UFDto

---
+++ backend/src/main/java/br/com/gustavomacedo/git_together/DTOs/UFDto.java
@@ -0,0 +1,15 @@
+package br.com.gustavomacedo.git_together.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UFDto {
+    private String uf;
+    private String name;
+}
