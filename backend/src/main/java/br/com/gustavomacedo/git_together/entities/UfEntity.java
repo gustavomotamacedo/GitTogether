f2987420799dbee75dfd8b5ec504c76bbea665b9
gustavomotamacedo <mota.macedo05@gmail.com>
2025-04-25 21:55:26 -0300

    feat: criando entidade de UF

---
+++ backend/src/main/java/br/com/gustavomacedo/git_together/entities/UfEntity.java
@@ -0,0 +1,19 @@
+package br.com.gustavomacedo.git_together.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_ufs")
+@Getter
+@Setter
+public class UfEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String uf;
+    private String name;
+}
