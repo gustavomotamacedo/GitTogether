b44944776e61438b654c86da9b136083dd6cab3b
gustavomotamacedo <mota.macedo05@gmail.com>
2025-04-25 21:57:07 -0300

    feat: criando entidade para interesses

---
+++ backend/src/main/java/br/com/gustavomacedo/git_together/entities/InterestEntity.java
@@ -0,0 +1,18 @@
+package br.com.gustavomacedo.git_together.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_interests")
+@Getter
+@Setter
+public class InterestEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+}
