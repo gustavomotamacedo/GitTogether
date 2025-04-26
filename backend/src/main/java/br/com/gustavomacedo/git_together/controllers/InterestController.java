f587a9f45eaa832ed9a38ab5f9cfdbec45cf11ea
gustavomotamacedo <mota.macedo05@gmail.com>
2025-04-26 00:02:08 -0300

    feat: adicionando mÃetodo e endpoint para retornar interesse por ID

--- backend/src/main/java/br/com/gustavomacedo/git_together/controllers/InterestController.java
+++ backend/src/main/java/br/com/gustavomacedo/git_together/controllers/InterestController.java
@@ -3,8 +3,8 @@ package br.com.gustavomacedo.git_together.controllers;
 import br.com.gustavomacedo.git_together.DTOs.InterestDto;
 import br.com.gustavomacedo.git_together.services.InterestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +17,14 @@ public class InterestController {
     @Autowired
     private InterestService interestService;
 
-
     @GetMapping
     public List<InterestDto> getAllInterests() {
         return interestService.getAllInterests();
+    }
+
+    @GetMapping("/{id}")
+    public InterestDto getInterestById(@PathVariable("id") Integer id) {
+        return interestService.getInterestById(id);
     }
 
 }
